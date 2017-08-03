30bb62b3b7dbf2af6c16576ac9fbce2c0f3b210f
Matthew Richards <Matthew.Richards@softwire.com>
2017-08-03 09:37:56 +0100

    Add migrations

---
+++ CatShelter.Migrations/CreateCatFoodStorage.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+
+namespace CatShelter.Migrations
+{
+  [Migration(20170131102400)]
+  public class CreateCatFoodStorage : Migration
+  {
+    public override void Up()
+    {
+      Create.Table("CatFoods")
+        .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+        .WithColumn("Name").AsString().NotNullable();
+
+      Insert.IntoTable("CatFoods")
+        .Row(new {Name = "Yummies"})
+        .Row(new {Name = "Crunchies"});
+
+      Create.Column("FavouriteFoodId").OnTable("Cats").AsInt32().Nullable()
+        .ForeignKey("FK_Cats_FavouriteFood", "CatFoods", "Id");
+    }
+
+    public override void Down()
+    {
+      Delete.ForeignKey("FK_Cats_FavouriteFood").OnTable("Cats");
+      Delete.Column("FavouriteFoodId").FromTable("Cats");
+      Delete.Table("CatFoods");
+    }
+  }
+}
