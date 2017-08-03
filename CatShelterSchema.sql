7369602fa6afb450dad18605c1941beaeb4273ac
Matthew Richards <Matthew.Richards@softwire.com>
2017-08-03 09:25:52 +0100

    Add foreign key relationship

--- CatShelterSchema.sql
+++ CatShelterSchema.sql
@@ -11,11 +11,23 @@ GO
 USE CatShelter
 GO
 
--- Create the Cats table
+-- Create the tables
 
 CREATE TABLE Cats (
 	Id int IDENTITY NOT NULL PRIMARY KEY,
-	Name nvarchar(max) NULL
+	Name nvarchar(max) NULL,
+	OwnerId int NOT NULL
 )
 GO
+
+CREATE TABLE Owners (
+	Id int IDENTITY NOT NULL PRIMARY KEY,
+	Name nvarchar(max) NOT NULL
+)
+GO
+
+ALTER TABLE Cats
+	ADD CONSTRAINT FK_Cats_Owner FOREIGN KEY (OwnerId) REFERENCES Owners(Id)
+GO
+
 
